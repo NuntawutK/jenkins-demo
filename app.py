bbcbc7f9a2b233e4a2473f3419e2957ab49eb5cb
Nantawut Kaangku <117341938+NuntawutK@users.noreply.github.com>
2025-05-01 03:57:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
+def add(a, b):
+    return a + b
 
-@app.route('/')
-def home():
-    return "Hello from CI/CD Pipeline!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+if __name__ == "__main__":
+    print("2 + 3 =", add(2, 3))
